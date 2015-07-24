9799928736a9e4ea82b430cfb130c67879e09fcc
Athan Clark <athan.clark@gmail.com>
2015-07-24 00:42:03 -0600

    fixed count

--- src/Data/Function/AlmostFix.hs
+++ src/Data/Function/AlmostFix.hs
@@ -4,15 +4,10 @@ import Control.Monad
 
 
 -- | Applies the predicate to the input:
--- @almostFix (< 5) (+1) 0  =  (((0+1)+1)+1)+1@
+-- @almostFix (< 5) (+1) 0  =  4@
 almostFix :: (a -> Bool) -> (a -> a) -> a -> a
 almostFix p f x = if p x then almostFix p f (f x)
                          else x
-
--- | Applies the predicate to the result.
-almostFix' :: (a -> Bool) -> (a -> a) -> a -> a
-almostFix' p f x = if p (f x) then almostFix' p f (f x)
-                              else x
 
 -- | Use a monadic predicate for the control flow.
 almostFixM :: Monad m => m Bool -> (a -> m a) -> a -> m a
